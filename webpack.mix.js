8182ad8f88b92b9c222af4770bc703f8bac538c0
Ranie Santos <ranietots32@gmail.com>
2018-08-15 15:31:59 +0800

    Flattened resources directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,14 +3,14 @@
 let mix = require('laravel-mix');
 
 mix
-    .js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/theme-light.scss', 'public/css')
-    .sass('resources/assets/sass/theme-dark.scss', 'public/css')
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/theme-light.scss', 'public/css')
+    .sass('resources/sass/theme-dark.scss', 'public/css')
     .webpackConfig({
         resolve: {
             alias: {
-                '@': path.resolve('resources/assets/js')
+                '@': path.resolve('resources/js')
             }
         },
         output: {
